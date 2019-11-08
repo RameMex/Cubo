00f614f7756181ba106071da4cfe2e99ca905963
Aldo Camacho <wannasz86@gmail.com>
2019-11-08 09:24:18 -0800

    Cubo en pantalla gira con respecto a cubo real

--- CuboComputadora/Javascript/js/serial.js
+++ CuboComputadora/Javascript/js/serial.js
@@ -10,24 +10,28 @@ const parser = new Readline({
 })
 port.pipe(parser)
 
-// Read data that is available but keep the stream in "paused mode"
-// port.on('readable', function () {
-//     console.log('Data:', port.read())
-// })
-
-// Switches the port into "flowing mode"
 parser.on('data', function (data) {
 	if (data == 1){
 		console.log('Verde')
+		updateRotate(0, -90, 0)
 	} else if (data == 2){
 		console.log('Azul')
+		updateRotate(90, 0, 0)
 	} else if (data == 3){
 		console.log('Rojo')
+		updateRotate(-90, 0, 0)
 	} else if (data == 4){
 		console.log('Cian')
+		updateRotate(0, 0, 0)
 	} else if (data == 5){
 		console.log('Amarillo')
+		updateRotate(0, 90, 0)
 	} else if (data == 6){
 		console.log('Morado')
+		updateRotate(0, -180, 0)
 	}
 })
+
+function updateRotate(x, y, z){
+	$('#cube').attr('style', 'transform: rotateX(' + x + 'deg) rotateY(' + y + 'deg) rotateZ(' + z + 'deg);')
+}
